6b28d2a36fffb1ee79d5c822c942661afac12327
Andrew Gaskell <gskll@protonmail.com>
2024-03-27 07:17:28 +0100

    cell draw_move with undo

--- src/models/cell.py
+++ src/models/cell.py
@@ -17,9 +17,11 @@ class Cell:
         top_right = Point(x + size, y)
         bottom_left = Point(x, y + size)
         bottom_right = Point(x + size, y + size)
+        center = Point(x + size // 2, y + size // 2)
 
         self.__wall_color = "green2"
         self.__window = window
+        self.__center_point = center
         self.__top_wall = Line(top_left, top_right)
         self.__right_wall = Line(top_right, bottom_right)
         self.__bottom_wall = Line(bottom_left, bottom_right)
@@ -41,3 +43,11 @@ class Cell:
 
         if self.has_left_wall:
             self.__window.draw_line(self.__left_wall, self.__wall_color)
+
+    def draw_move(self, to_cell: "Cell", undo=False) -> None:
+        move_line_color = self.__wall_color
+        if undo:
+            move_line_color = "red"
+
+        move_line = Line(self.__center_point, to_cell.__center_point)
+        self.__window.draw_line(move_line, move_line_color)
