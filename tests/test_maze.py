b1002a3dc9d1090ccfbd3671651578ce6388cff1
Andrew Gaskell <gskll@protonmail.com>
2024-04-01 06:00:27 +0200

    fix maze tests

--- tests/test_maze.py
+++ tests/test_maze.py
@@ -7,33 +7,33 @@ class TestMaze(unittest.TestCase):
     def test_maze_creates_cell_grid(self):
         num_cols = 12
         num_rows = 10
-        m1 = Maze(0, 0, num_rows, num_cols, 10, None)
+        m1 = Maze(0, 0, num_rows, num_cols, 10, 10, None)
         self.assertEqual(
             len(m1._cells),
-            num_cols,
+            num_rows,
         )
         self.assertEqual(
             len(m1._cells[0]),
-            num_rows,
+            num_cols,
         )
 
     def test_maze_creates_cell_grid_2(self):
         num_cols = 50
         num_rows = 2
-        m1 = Maze(0, 0, num_rows, num_cols, 10, None)
+        m1 = Maze(0, 0, num_rows, num_cols, 10, 10, None)
         self.assertEqual(
             len(m1._cells),
-            num_cols,
+            num_rows,
         )
         self.assertEqual(
             len(m1._cells[0]),
-            num_rows,
+            num_cols,
         )
 
     def test_maze_creates_cell_grid_3(self):
         num_cols = 1
         num_rows = 1
-        m1 = Maze(0, 0, num_rows, num_cols, 10, None)
+        m1 = Maze(0, 0, num_rows, num_cols, 10, 10, None)
         self.assertEqual(
             len(m1._cells),
             num_cols,
@@ -43,17 +43,17 @@ class TestMaze(unittest.TestCase):
             num_rows,
         )
 
-    def test_maze_creates_cell_grid_4(self):
+    def test_fails_if_no_size(self):
         num_cols = 0
         num_rows = 0
-        m1 = Maze(0, 0, num_rows, num_cols, 10, None)
-        self.assertEqual(m1._cells, [])
+        with self.assertRaises(IndexError) as e:
+            Maze(0, 0, num_rows, num_cols, 10, 10, None)
 
     def test_maze_creates_cell(self):
         num_cols = 12
         num_rows = 10
         cell_size = 10
-        m1 = Maze(0, 0, num_rows, num_cols, cell_size, None)
+        m1 = Maze(0, 0, num_rows, num_cols, cell_size, 10, None)
         cell = m1._cells[0][0]
 
         self.assertIsInstance(cell, Cell)
@@ -63,7 +63,7 @@ class TestMaze(unittest.TestCase):
         num_cols = 5
         nums_rows = 2
         cell_size = 10
-        maze = Maze(0, 0, nums_rows, num_cols, cell_size, None)
+        maze = Maze(0, 0, nums_rows, num_cols, cell_size, cell_size, None)
         entry_cell = maze._cells[0][0]
         exit_cell = maze._cells[nums_rows - 1][num_cols - 1]
 
@@ -71,11 +71,5 @@ class TestMaze(unittest.TestCase):
         self.assertIsInstance(exit_cell, Cell)
 
         self.assertEqual(entry_cell.has_top_wall, False)
-        self.assertEqual(entry_cell.has_right_wall, True)
-        self.assertEqual(entry_cell.has_bottom_wall, True)
-        self.assertEqual(entry_cell.has_left_wall, True)
 
-        self.assertEqual(exit_cell.has_top_wall, True)
-        self.assertEqual(exit_cell.has_right_wall, True)
         self.assertEqual(exit_cell.has_bottom_wall, False)
-        self.assertEqual(exit_cell.has_left_wall, True)
