6455bc27eed2ae3bbc8e02c2686c414592df01e2
Andrew Gaskell <gskll@protonmail.com>
2024-04-09 06:24:58 +0200

    add default button class

---
+++ src/button.py
@@ -0,0 +1,31 @@
+from tkinter import Frame, font, LEFT
+from typing import Callable
+from tkmacosx import Button
+
+
+class Btn:
+    def __init__(
+        self,
+        frame: Frame,
+        text: str,
+        command: Callable,
+        main_color: str,
+        bg_color: str,
+    ):
+        helv16 = font.Font(family="Helvetica", size=16, weight=font.BOLD)
+        self._btn = Button(
+            frame,
+            text=text,
+            font=helv16,
+            command=command,
+            bordercolor=main_color,
+            foreground=main_color,
+            background=bg_color,
+            activeforeground=bg_color,
+            activebackground=main_color,
+            disabledforeground=bg_color,
+            disabledbackground=bg_color,
+            highlightcolor=bg_color,
+            height=50,
+        )
+        self._btn.pack(side=LEFT, padx=10, pady=10)
