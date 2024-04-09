bf38e03c767ead7af46f6deee465d2afbce4ba59
Andrew Gaskell <gskll@protonmail.com>
2024-04-09 06:26:37 +0200

    edit maze to use new Window with buttons

--- src/main.py
+++ src/main.py
@@ -1,22 +1,11 @@
-from maze import Maze
 from window import Window
 
 
 def main():
-    margin = 50
-    num_rows = 12
-    num_cols = 16
     screen_x = 800
     screen_y = 600
-    cell_size_x = (screen_x - 2 * margin) // num_cols
-    cell_size_y = (screen_y - 2 * margin) // num_rows
 
     win = Window(screen_y, screen_x)
-    maze = Maze(
-        margin, margin, num_rows, num_cols, cell_size_x, cell_size_y, window=win
-    )
-    maze.solve()
-
     win.wait_for_close()
 
 
