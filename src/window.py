436fa8b9ebd29fcaa637a0dee4ecba7ce1eacb6f
Andrew Gaskell <gskll@protonmail.com>
2024-04-10 03:51:43 +0200

    add window lock decorator

--- src/window.py
+++ src/window.py
@@ -6,8 +6,27 @@ from line import Line
 from maze import Maze
 
 
+def lock(func):
+    def wrapper(self, *args, **kwargs):
+        if self._is_working:
+            print(f"cannot run {func.__name__} while working")
+            return
+
+        self._is_working = True
+        try:
+            result = func(self, *args, **kwargs)
+        except Exception as e:
+            raise e
+        finally:
+            self._is_working = False
+        return result
+
+    return wrapper
+
+
 class Window:
     def __init__(self, height: int, width: int):
+        self._is_working = False
         self._running = False
         self._main_color = "green"
         self._bg_color = "gray3"
@@ -25,27 +44,44 @@ class Window:
         self._make_buttons()
         self._setup_maze(height, width)
 
-    def _make_buttons(self):
-        self._button_frame = Frame(self._root, bg=self._bg_color)
-        self._button_frame.pack(fill=X, side=TOP, padx=10, pady=10)
+    def redraw(self):
+        self._root.update_idletasks()
+        self._root.update()
 
-        Btn(
-            self._button_frame,
-            "New Path",
-            self._new_path,
-            self._main_color,
-            self._bg_color,
-        )
+    def wait_for_close(self):
+        self._running = True
+        while self._running:
+            self.redraw()
+        print("Window closed.")
 
-        Btn(
-            self._button_frame,
-            "Solve - DFS",
-            self._dfs,
-            self._main_color,
-            self._bg_color,
-        )
+    def close(self):
+        self._running = False
 
+    def draw_line(self, line: Line, fill_color: str):
+        line.draw(self._canvas, fill_color)
+
+    def animate(self):
+        self.redraw()
+        time.sleep(0.01)
+
+    @lock
+    def _solve_dfs(self):
+        print("solving dfs")
+        self._maze.solve_dfs()
+
+    def _solve_bfs(self):
+        print("solving bfs")
+
+    @lock
+    def _new_path(self):
+        print("new maze path")
+        self._canvas.delete("all")
+        self.redraw()
+        self._maze.make_path()
+
+    @lock
     def _setup_maze(self, screen_y, screen_x):
+        print("setting up maze")
         margin = 50
         num_rows = 12
         num_cols = 16
@@ -67,34 +103,22 @@ class Window:
         )
         self._maze.make_path()
 
-    def _new_path(self):
-        self._canvas.delete("all")
-        self.redraw()
-        self._maze.make_path()
+    def _make_buttons(self):
+        self._button_frame = Frame(self._root, bg=self._bg_color)
+        self._button_frame.pack(fill=X, side=TOP, padx=10, pady=10)
 
-    def _dfs(self):
-        self._maze.solve_dfs()
+        Btn(
+            self._button_frame,
+            "New Path",
+            self._new_path,
+            self._main_color,
+            self._bg_color,
+        )
 
-    def _solve_bfs(self):
-        print("solve bfs")
-
-    # buttons: animate maze drawing, solve maze, new maze
-    def redraw(self):
-        self._root.update_idletasks()
-        self._root.update()
-
-    def wait_for_close(self):
-        self._running = True
-        while self._running:
-            self.redraw()
-        print("Window closed.")
-
-    def close(self):
-        self._running = False
-
-    def draw_line(self, line: Line, fill_color: str):
-        line.draw(self._canvas, fill_color)
-
-    def animate(self):
-        self.redraw()
-        time.sleep(0.01)
+        Btn(
+            self._button_frame,
+            "Solve - DFS",
+            self._solve_dfs,
+            self._main_color,
+            self._bg_color,
+        )
